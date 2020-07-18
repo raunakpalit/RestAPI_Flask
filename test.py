c8b76cb2686c105ee6fea30fba22c3e711ba5cbf
Raunak <palit.raunak@gmail.com>
2020-07-18 22:34:51 +0530

    Added test.py

--- test.py
+++ test.py
@@ -1,3 +1,67 @@
-print("Hi there. My name is Raunak Palit")
+////////////////////////
+// Copyright
+// THIS MATERIAL
+// DISCLOSURE
+// BY
+////////////////////////
+/***********************
+* Desc
+* Class
+*
+*
+************************
 
-age = 31
+#include "CANNameAPI.h"
+#include "lkdjds"
+
+using namespace CommsBoard;
+
+CANNameAPI::CANName()
+:Mfg
+{
+}
+
+bool CANNameAPI: Get
+{
+   bool ret = false;
+   if(iso)
+   {
+   Uint industry;
+   something;
+   }
+}
+
+bool CANNameAPI: Get
+{
+   bool ret = false;
+   if(iso)
+   {
+   Uint industry;
+   something;
+   }
+}
+
+bool CANNameAPI::GET
+{
+    msg.clear();
+    if (!version)
+    {
+        const char* prefix = "SW0001";
+        const char* partNumber = "PFP15880W";
+
+        std::string;
+        msg;
+        msgStr;
+    }
+    return
+}
+
+bool CANNameAPI: Get
+{
+   bool ret = false;
+   if(iso)
+   {
+   Uint industry;
+   something;
+   }
+}
